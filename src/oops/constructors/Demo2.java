6c024f3d5d85f58fdf48de3aa751e14a628a5556
automationcatalogue <automation.catalogue@gmail.com>
2023-07-21 17:00:48 +0530

    Day10-Java OOPS Constructors and Inheritance

---
+++ src/oops/constructors/Demo2.java
@@ -0,0 +1,10 @@
+package oops.constructors;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Emp1 e1 = new Emp1(101, 10000.00f);
+        System.out.println(e1.empNo+"-"+e1.empSalary);
+
+
+    }
+}
---
+++ src/oops/constructors/Emp.java
@@ -0,0 +1,7 @@
+package oops.constructors;
+
+// This Program - Initialization of instance variables without constructors
+public class Emp {
+    int empNo=101;
+    float empSalary=10000.00f;
+}
