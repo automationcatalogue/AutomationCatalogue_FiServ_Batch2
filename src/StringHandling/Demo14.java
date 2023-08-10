9da263c2ea89cdeb949945233165c7b6fe12162d
automationcatalogue <automation.catalogue@gmail.com>
2023-08-10 15:32:27 +0530

    Day19-Java String Handling and Wrapper Classes

---
+++ src/StringHandling/Demo14.java
@@ -0,0 +1,11 @@
+package StringHandling;
+
+public class Demo14 {
+    public static void main(String[] args) {
+        int a ="Hello India".length();
+        System.out.println(a);
+
+        String s2="Hello India".toUpperCase();
+        System.out.println(s2);
+    }
+}
---
+++ src/wrapperClasses/Demo1.java
@@ -0,0 +1,13 @@
+package wrapperClasses;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int x=5;
+        Integer ob1 = new Integer(x);
+        System.out.println(ob1);
+
+        char ch='J';
+        Character ob2 = new Character(ch);
+        System.out.println(ob2);
+    }
+}
