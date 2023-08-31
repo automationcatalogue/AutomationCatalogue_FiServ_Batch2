c1ab4a06b9350da9f2247700a1b4a8c6bfa2b079
automationcatalogue <automation.catalogue@gmail.com>
2023-08-31 09:18:09 +0530

    Day25-Java Collections Map and MultiThreading

--- src/CollectionFramework/HSDemo1.java
+++ src/CollectionFramework/HSDemo1.java
@@ -8,38 +8,8 @@ public class HSDemo1 {
         ArrayList<String> al1 = new ArrayList<String>(Arrays.asList(empNames));
         System.out.println(al1);
 
-        Integer empSalaries[] ={25000,40000};
-        ArrayList<Integer> al2 = new ArrayList<Integer>(Arrays.asList(empSalaries));
-        System.out.println(al2);
-
-        Collections.sort(al1);
-        System.out.println(al1);
-
-        Character empGenders[]={'M','F','M'};
-        ArrayList<Character> al3 = new ArrayList<Character>(Arrays.asList(empGenders));
-        System.out.println(al3);
-
-        ArrayList al = new ArrayList();
-        al.addAll(al1);
-        al.addAll(2,al2);
-        al.addAll(3,al3);
-        System.out.println(al);
-
-        al.add(25000);
-        al.add("Srinath");
-        System.out.println(al);
-
-        HashSet hs = new HashSet(al);
-        System.out.println(hs);
-
-        int size = hs.size();
-        System.out.println(size);
-
-        hs.remove(25000);
-        System.out.println(hs);
-
-        LinkedHashSet lhs = new LinkedHashSet(al);
-        System.out.println(lhs);
-
+        for(String s:al1){
+            System.out.println(s);
+        }
     }
 }
---
+++ src/MultiThreading/TDemo3.java
@@ -0,0 +1,35 @@
+package MultiThreading;
+
+public class TDemo3 extends Thread{
+    public void run() {
+        String empName[]={"Soniya","Mohan","Lipsa","Arun","Rajakumar","Sagar","Automation","Catalogue"};
+        for(String s:empName){
+            System.out.println("Child Thread Task Code :"+s);
+
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException ie){
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args)  throws Exception{
+        TDemo3 ob = new TDemo3();
+        ob.start();
+
+        for(int i=1;i<=10;i++){
+            System.out.println("Main Thread task "+i);
+            Thread.sleep(1000);
+
+            if(i==5){
+                ob.suspend();
+            }
+
+            if(i==10){
+                ob.resume();
+            }
+        }
+    }
+}
