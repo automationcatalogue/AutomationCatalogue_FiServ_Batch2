8c9106fedd1635263d7a38eb24f386a3a0212697
RahulChauhanGIT <95597159+RahulChauhanGIT@users.noreply.github.com>
2024-03-10 06:24:26 -0400

    Add files via upload

---
+++ src/ClassRoom.java
@@ -0,0 +1,19 @@
+package Assessment3_Trainer;
+public class ClassRoom {
+    private int classId;
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public char[] getClassNames() {
+        return classNames;
+    }
+
+    private char classNames[];
+
+    ClassRoom(int a, char ch[]){
+        this.classId=a;
+        this.classNames=ch;
+    }
+}
